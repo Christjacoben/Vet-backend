8607957cfbd08ebf85e6f311b65b698cc590de43
Cris <111942487+Christjacoben@users.noreply.github.com>
2025-01-12 22:55:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -273,8 +273,8 @@ app.post("/api/login", async (req, res) => {
     );
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "None",
     });
     res.status(200).json({
       message: "Logged in successfully",
